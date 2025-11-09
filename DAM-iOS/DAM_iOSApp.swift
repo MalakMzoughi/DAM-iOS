b7b278db936813e673b6041c5f6b759e93e9a51b
essidss <malak.mzoughi@esprit.tn>
2025-11-09 18:51:19 +0100

    Initial Commit

---
+++ DAM-iOS/DAM_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAM_iOSApp.swift
+//  DAM-iOS
+//
+//  Created by iMac on 9/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct DAM_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
